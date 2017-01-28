7976a4772fd82468e6ee0d586ec98218683298c0
fgmaion <fgmaion@gmail.com>
2017-01-28 12:39:02 -0200

    new commit

--- Francisco/ParticleSystem2D.cpp
+++ Francisco/ParticleSystem2D.cpp
@@ -9,12 +9,6 @@
 
 using namespace std;
 
-/*double d(Particle* p1, Particle* p2)
-{
-	//double d = sqrt(pow((p1->x_t[0]-p2->x_t[0]),2)+pow((p1->x_t[1]-p2->x_t[1]),2));
-	return d;
-}*/
-
 class ParticleSystem
 	{
 	public:
@@ -38,6 +32,7 @@ ParticleSystem::ParticleSystem()
 {
 }	
 
+//FRONTEIRA CHECA O SENTIDO DA VELOCIDADE E TROCA, SE NECESSARIO
 void ParticleSystem::Boundaries()
 {
 	for (int i =0; i < 1000; i++)
@@ -45,23 +40,36 @@ void ParticleSystem::Boundaries()
 				Particle* p1 = &Particles[i];
 				if (p1->GetX0() > 1)
 					{
-						p1->velo_t[0] = -1*p1->velo_t[0];
+						if (p1->GetV0() > 0)
+						{
+							p1->SetV0(-1*p1->GetX0());
+						}
 					}
 				if (p1->GetX0()<-1)
-					{
-						p1->velo_t[0] = -1*p1->velo_t[0];
+					{	
+						if (p1->GetV0() < 0)
+						{
+							p1->SetV0(-1*p1->GetV0());
+						}
 					}
 				if (p1->GetX1() > 1)
 					{
-						p1->velo_t[1] = -1*p1->velo_t[1];
+						if (p1->GetV1()>0)
+						{
+							p1->SetV1(-1*p1->GetV1());
+						}
 					}
 				if (p1->GetX1() < -1)
 					{
-						p1->velo_t[1] = -1*p1->velo_t[1];
+						if (p1->GetV1()<0)
+						{
+							p1->SetV1(-1*p1->GetV1());
+						}
 					}
 	}
 }
-		
+
+//INICIALIZA ALEATORIAMENTE A POSICAO DAS PARTICULAS NUM QUADRADO DE LADO 2		
 void ParticleSystem::initialize()
 {
 	srand(time(NULL));
@@ -75,12 +83,11 @@ void ParticleSystem::initialize()
 				p1->SetX0((rand() % 20000)/10000.0 - 1);
 				p1->SetX1((rand() % 20000)/10000.0 - 1);
 				
-				p1->velo_t[0] = 0;
-				p1->velo_t[1] = 0;
+				p1->SetV0(0);
+				p1->SetV1(0);
 				
-				p1->a_t[0] = 0;
-				p1->a_t[1] = 0;
-// INITIALIZE FOI TESTADA ESTA FUNCIONANDO BEM
+				p1->SetA0(0);
+				p1->SetA1(0);
 	}
 }
 
@@ -89,24 +96,27 @@ double ParticleSystem::CalculateDensity(Particle* p1, double h)
 double A = 0;
 for(int i =0; i<1000; i++)
 {
-						//A = A + Particles[i].GetMass()*Kernel2D(sqrt(pow((p1->x_t[0]-Particles[i].x_t[0]),2)+pow((p1->x_t[1]-Particles[i].x_t[1]),2))/h,h);
-						cout << "A " << A << " d " << Particles[i].GetX1();
+	if (Particles[i].GetX0() > (p1->GetX0()-2*h) && Particles[i].GetX0() < (p1->GetX0()+2*h))
+			{
+				if (Particles[i].GetX1()> p1->GetX1()-2*h && Particles[i].GetX1() < p1->GetX1()+2*h)
+				{
+						A = A + Particles[i].GetMass()*Kernel2D(sqrt(pow((p1->GetX0()-Particles[i].GetX0()),2)+pow((p1->GetX1()-Particles[i].GetX1()),2))/h,h);
+				}
+			}
 }
 return A;
 }
-//kernel esta ok
-/*
+
 double ParticleSystem::CalculateDensityGradX(Particle* p1,double h)
 {
 double A =0;
 for (int i =0; i <1000; i++)
 {
-		if (Particles[i].x_t[0] > (p1->x_t[0]-2*h) && Particles[i].x_t[0] < (p1->x_t[0]+2*h))
+	if (Particles[i].GetX0() > (p1->GetX0()-2*h) && Particles[i].GetX0() < (p1->GetX0()+2*h))
 			{
-				if (Particles[i].x_t[1] > p1->x_t[1]-2*h && Particles[i].x_t[1] < p1->x_t[1]+2*h)
-				{	
-					//cout << "posicao " << p1->x_t[0] << " grad "   << GradKernel2DX(p1->x_t[0]-Particles[i].x_t[0],p1->x_t[1]-Particles[i].x_t[1],h) << "\n";
-					A = A + Particles[i].GetMass()*GradKernel2DX(p1->x_t[0]-Particles[i].x_t[0],p1->x_t[1]-Particles[i].x_t[1],h);
+				if (Particles[i].GetX1()> p1->GetX1()-2*h && Particles[i].GetX1() < p1->GetX1()+2*h)
+				{
+					A = A + Particles[i].GetMass()*GradKernel2DX(p1->GetX0()-Particles[i].GetX0(),p1->GetX1()-Particles[i].GetX1(),h);
 				}
 			}
 }
@@ -118,33 +128,35 @@ double ParticleSystem::CalculateDensityGradY(Particle* p1,double h)
 double A =0;
 for (int i =0; i <1000; i++)
 {
-		if (Particles[i].x_t[0] > (p1->x_t[0]-2*h) && Particles[i].x_t[0] < (p1->x_t[0]+2*h))
+	if (Particles[i].GetX0() > (p1->GetX0()-2*h) && Particles[i].GetX0() < (p1->GetX0()+2*h))
 			{
-				if (Particles[i].x_t[1] > (p1->x_t[1]-2*h) && Particles[i].x_t[1] < (p1->x_t[1]+2*h))
+				if (Particles[i].GetX1()> p1->GetX1()-2*h && Particles[i].GetX1() < p1->GetX1()+2*h)
 				{
-						A = A + Particles[i].GetMass()*GradKernel2DY(p1->x_t[0]-Particles[i].x_t[0],p1->x_t[1]-Particles[i].x_t[1],h);
+					A = A + Particles[i].GetMass()*GradKernel2DY(p1->GetX0()-Particles[i].GetX0(),p1->GetX1()-Particles[i].GetX1(),h);
 				}
 			}
 }
 return A;
 }
-*/
+
 void ParticleSystem::Step(double dt)
 {
 	for (int i = 0; i< 1000; i++)
 		{
 				Particle* p1 = &Particles[i];
-				double A1 = p1->a_t[0];
-				double A2 = p1->a_t[1];
+				double A1 = p1->GetA0();
+				double A2 = p1->GetA1();
 	
-				p1->a_t[0] = -(0.5)*CalculateDensityGradX(p1,0.12) - p1->GetX0()-0.2*(p1->velo_t[0]);
-				p1->a_t[1] = -(0.5)*CalculateDensityGradY(p1,0.12) - p1->GetX1()-0.2*(p1->velo_t[1]);
+				p1->SetA0(-(0.5)*CalculateDensityGradX(p1,0.12) - p1->GetX0()-0.2*(p1->GetV0()));
+				p1->SetA1(-(0.5)*CalculateDensityGradY(p1,0.12) - p1->GetX1()-0.2*(p1->GetV1()));
 				
-				p1->velo_t[0] = p1->velo_t[0] + 0.5*(A1 + p1->a_t[0])*dt;
-				p1->velo_t[1] = p1->velo_t[1] + 0.5*(A2 + p1->a_t[1])*dt;
+				p1->SetV0(p1->GetV0() + 0.5*(A1 + p1->GetA0())*dt);
+				p1->SetV1(p1->GetV1() + 0.5*(A2 + p1->GetA1())*dt);
 				
-				p1->SetX0(p1->GetX0() + p1->velo_t[0]*dt + 0.5*A1*pow(dt,2));
-				p1->SetX1(p1->GetX1() + p1->velo_t[1]*dt + 0.5*A2*pow(dt,2));
+				p1->SetX0(p1->GetX0() + p1->GetV0()*dt + 0.5*A1*pow(dt,2));
+				p1->SetX1(p1->GetX1() + p1->GetV1()*dt + 0.5*A2*pow(dt,2));
+				
+				//ITERACAO TEMPORAL NO METODO LEAPFROG FEITO EM UMA UNICA ETAPA
 			}
 }
 
@@ -153,45 +165,26 @@ int main()
 {
 ParticleSystem * PS1 = new ParticleSystem;
 PS1->initialize();
-for (int i = 0; i<200; i++)
+for (int i = 0; i<1000; i++)
 	{
 		
 		PS1->Step(0.01);
 		PS1->Boundaries();
 	}
-for (int j =0; j<2; j++)
-{
-	Particle* p1= &PS1->Particles[j];
-	cout << "densidade" << PS1->CalculateDensity(p1,0.1) << endl;
-	
-}
 
-/*
+	
+
+
+//EXPORTA OS DADOS PARA UM ARQUIVO
 ofstream myfile2 ("/home/francisco/myfile2.txt");
 if (myfile2.is_open())
 {
 	for (int j = 0; j<1000; j++)
 		{
-		myfile2 << PS1->Particles[j].x_t[0] << "," << PS1->Particles[j].x_t[1] << "," << PS1->CalculateDensity(&PS1->Particles[j],0.12) << "\n";
+		
+		myfile2 << PS1->Particles[j].GetX0()<< "," << PS1->Particles[j].GetX1() << "," << PS1->CalculateDensity(&PS1->Particles[j],0.12) << "\n";
 		}
 myfile2.close();
 }
-*/
+
 }
-
-
-		
-		
-		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
